4436c3cf34f38e6c212ae956c662a7cf7d72ec41
Alex <alex@localhost.localdomain>
2014-01-15 19:22:44 +0100

    ajout de la table point_type à la bdd

--- database/gps-server.sql
+++ database/gps-server.sql
@@ -28,6 +28,10 @@ CREATE TABLE public.person(
 
 );
 -- ddl-end --
+COMMENT ON TABLE public.person IS 'participant, organisateur, staff, examinateur, secouriste';
+-- ddl-end --
+-- ddl-end --
+
 -- object: public.team | type: TABLE --
 CREATE TABLE public.team(
 	id smallint,
@@ -41,11 +45,15 @@ CREATE TABLE public.team(
 -- object: public.role | type: TABLE --
 CREATE TABLE public.role(
 	id smallint,
-	name varchar(16),
+	name varchar(16) NOT NULL,
 	CONSTRAINT pk3 PRIMARY KEY (id)
 
 );
 -- ddl-end --
+COMMENT ON TABLE public.role IS 'participant, organisateur, secouriste, staff,...';
+-- ddl-end --
+-- ddl-end --
+
 -- object: public.tracker | type: TABLE --
 CREATE TABLE public.tracker(
 
@@ -64,6 +72,10 @@ ALTER TABLE public.tracker ADD COLUMN manufacturer varchar(32);
 ALTER TABLE public.tracker ADD COLUMN model varchar(32);
 -- ddl-end --
 
+-- object: imei | type: COLUMN --
+ALTER TABLE public.tracker ADD COLUMN imei smallint;
+-- ddl-end --
+
 -- object: id_person | type: COLUMN --
 ALTER TABLE public.tracker ADD COLUMN id_person smallint;
 -- ddl-end --
@@ -76,6 +88,12 @@ ALTER TABLE public.tracker ADD CONSTRAINT pk4 PRIMARY KEY (id);
 -- ddl-end --
 
 
+COMMENT ON TABLE public.tracker IS 'boitier de géolocalisation';
+-- ddl-end --
+COMMENT ON COLUMN public.tracker.imei IS 'identifiant du modem mobile';
+-- ddl-end --
+-- ddl-end --
+
 -- object: public.sim | type: TABLE --
 CREATE TABLE public.sim(
 	id smallint,
@@ -87,6 +105,12 @@ CREATE TABLE public.sim(
 
 );
 -- ddl-end --
+COMMENT ON TABLE public.sim IS 'carte SIM opérateur';
+-- ddl-end --
+COMMENT ON COLUMN public.sim.model IS 'carte SIM, normale, mini ou micro';
+-- ddl-end --
+-- ddl-end --
+
 -- object: public.location | type: TABLE --
 CREATE TABLE public.location(
 	tracker public.tracker,
@@ -102,24 +126,35 @@ CREATE TABLE public.location(
 -- object: public.poi | type: TABLE --
 CREATE TABLE public.poi(
 	id smallint,
+	rank smallint,
 	name varchar(32),
 	latitude double precision,
 	longitude double precision,
 	altitude double precision,
 	id_path smallint,
+	id_point_type smallint,
 	CONSTRAINT pk2 PRIMARY KEY (id)
 
 );
 -- ddl-end --
+COMMENT ON TABLE public.poi IS 'point d''intérêt, lieu, départ,arrivée, waypoint, checkpoint, ravitaillement';
+-- ddl-end --
+-- ddl-end --
+
 -- object: public.path | type: TABLE --
 CREATE TABLE public.path(
 	id smallint,
 	name varchar(32),
 	description varchar(128),
+	time date,
 	CONSTRAINT pk1 PRIMARY KEY (id)
 
 );
 -- ddl-end --
+COMMENT ON TABLE public.path IS 'chemin, itinéraire';
+-- ddl-end --
+-- ddl-end --
+
 -- object: tracker_fk | type: CONSTRAINT --
 ALTER TABLE public.location ADD CONSTRAINT tracker_fk FOREIGN KEY (id_tracker)
 REFERENCES public.tracker (id) MATCH FULL
@@ -174,6 +209,30 @@ ON DELETE SET NULL ON UPDATE CASCADE NOT DEFERRABLE;
 
 -- object: tracker_uq1 | type: CONSTRAINT --
 ALTER TABLE public.tracker ADD CONSTRAINT tracker_uq1 UNIQUE (id_sim);
+-- ddl-end --
+
+
+-- object: public.point_type | type: TABLE --
+CREATE TABLE public.point_type(
+	id smallint,
+	name varchar(32) NOT NULL,
+	CONSTRAINT "pkPP" PRIMARY KEY (id)
+
+);
+-- ddl-end --
+COMMENT ON TABLE public.point_type IS 'départ, arrivée, checkpoint, ravitaillement,...';
+-- ddl-end --
+-- ddl-end --
+
+-- object: point_type_fk | type: CONSTRAINT --
+ALTER TABLE public.poi ADD CONSTRAINT point_type_fk FOREIGN KEY (id_point_type)
+REFERENCES public.point_type (id) MATCH FULL
+ON DELETE RESTRICT ON UPDATE CASCADE NOT DEFERRABLE;
+-- ddl-end --
+
+
+-- object: poi_uq | type: CONSTRAINT --
+ALTER TABLE public.poi ADD CONSTRAINT poi_uq UNIQUE (id_point_type);
 -- ddl-end --
 
 
