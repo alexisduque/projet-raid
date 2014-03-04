fd5e12c8a1e7e434cab959461e202936d7956c99
iso <iso@DSI401-A007.insa-lyon.fr>
2014-03-04 20:48:44 +0100

    url

--- web/gps/gps/urls.py
+++ web/gps/gps/urls.py
@@ -6,12 +6,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'gps.views.home', name='home'),
-    # url(r'^gps/', include('gps.foo.urls')),
+     url(r'^$', 'gps.views.home', name='home'),
+     url(r'^gps/', include('gps.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     url(r'^/admin/', include(admin.site.urls)),
+     url(r'^admin/', include(admin.site.urls)),
 )
