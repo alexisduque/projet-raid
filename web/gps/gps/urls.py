7a0ffbcf5c174c8e4823815bc3fa8a0f20a5cb46
iso <iso@DSI401-A007.insa-lyon.fr>
2014-03-08 10:12:37 +0100

    probleme urls

--- web/gps/gps/urls.py
+++ web/gps/gps/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Geoloc.views.home', name='home'),
-    # url(r'^gps/', include('gps.foo.urls')),
+     url(r'^gps/', include('gps.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
      #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
