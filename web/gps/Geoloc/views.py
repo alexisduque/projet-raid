aae049253aadc6a5cd894aad1c84ba0e5bce5a97
Baptiste Desbiolles <bapt.desbiolles@gmail.com>
2014-03-30 22:48:09 +0200

    corrections d'erreurs détectées dans les views.py.

--- web/gps/Geoloc/views.py
+++ web/gps/Geoloc/views.py
@@ -1,14 +1,17 @@
 # Create your views here.
 from django.http import HttpResponse
+
+
 def home(request):
-  text = """<h1>Bienvenue sur notre site web !</h1>
+    text = """<h1>Bienvenue sur notre site web !</h1>
             <p>Les crepes bretonnes ça tue des mouettes en plein vol !</p>"""
-  return HttpResponse(text)
+    return HttpResponse(text)
 
 from django.shortcuts import render_to_response
 from models import Tracker
 
-def last_positions(request)
-	liste_trackers = Tracker.objects.order_by('created_date')[10]
-	return render_to_response('last_positions.html,{'liste_trackers':liste_trackers})
+
+def last_positions(request):
+    liste_trackers = Tracker.objects.order_by('created_date')[10]
+    return render_to_response('last_positions.html', {'liste_trackers': liste_trackers})
 
